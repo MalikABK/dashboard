0fa5c3e006a8a0d53a46f7575507b22d9baa6113
MalikABK <malikallahbuhksh@gmail.com>
2024-12-02 02:49:07 -0800

    pushing the code on github

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default  function Page(){
+    return (
+        <div>
+            <p>this is a customer page.</p>
+        </div>
+    );
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function page(){
+    return (
+        <div>
+            <p>This is invoice pages</p>
+        </div>
+    );
+}
